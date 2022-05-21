d5b41199234ec2f6041d05fe1c4545a86ebc5b34
Steven <stevendawn98@gmail.com>
2022-05-21 13:54:23 -0400

    commit tree 820455fc3aaff2c0888cffc74877c5116dc5dcb0

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const divide = function(a, b) {
-    return a / b;
-}
+// const divide = function(a, b) {
+//     return a / b;
+// }
+
+const divide = (a, b) => a / b;
+
+const square = (x) => x * x;
+
+const add = (a, b) => a + b;
